78788ca00131931fc7b910fac9e8386673ea281f
algazza <fadhilaswadira72564@gmail.com>
2024-08-06 09:22:03 +0700

    hoi

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import SyaratPage from './Components/SyaratForm/SyaratPage.jsx';
-import Kredit from './Components/Form/Kredit.jsx';
-import KreditPage from './Pages/KreditPage.jsx';
-import LocationPage from './Components/location/LocationPage.jsx';
-import News from './Pages/News.jsx';
-import Success from './Components/Success.jsx';
-import PickupPage from './Pages/PickupPage.jsx';
-import SyaratForm from './Components/SyaratForm.jsx';
-import MergedTable from './Components/tabel.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import SyaratPage from "./Components/SyaratForm/SyaratPage.jsx";
+import Kredit from "./Components/Form/Kredit.jsx";
+import KreditPage from "./Pages/KreditPage.jsx";
+import News from "./Pages/News.jsx";
+import Success from "./Components/Success.jsx";
+import PickupPage from "./Pages/PickupPage.jsx";
+import SyaratForm from "./Components/SyaratForm.jsx";
+import MergedTable from "./Components/tabel.jsx";
 
 const router = createBrowserRouter([
   {
@@ -30,18 +29,12 @@ const router = createBrowserRouter([
     element: <KreditPage />,
   },
   {
-<<<<<<< HEAD
     path: "/pickuppage",
     element: <PickupPage />,
-=======
-    path: "/Location",
-    element: <LocationPage />,
->>>>>>> c9cd972808bfd909ef49865a5e4f0a7c1533a913
   },
   {
     path: "/news",
     element: <News />,
-<<<<<<< HEAD
   },
   {
     path: "/succes",
@@ -55,14 +48,10 @@ const router = createBrowserRouter([
     path: "/table",
     element: <MergedTable />,
   },
-=======
-  }
->>>>>>> c9cd972808bfd909ef49865a5e4f0a7c1533a913
-
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
