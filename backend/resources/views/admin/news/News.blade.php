dacf848b67d11dbfaa1413b682d2df0c06e6fb20
howienathan <jonathanaugta3@gmail.com>
2024-08-08 10:11:45 +0700

    start 08-08-2024

--- backend/resources/views/admin/news/News.blade.php
+++ backend/resources/views/admin/news/News.blade.php
@@ -48,90 +48,20 @@
 </div>
   </div>
   <div class="flex gap-[1.5rem] pt-[5rem] pb-[2rem] px-[10rem] bg-gray-200 w-screen">
-                    <img class=" bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShPq0XGv-4wIxCVB_qHi42ZqrmFr-Aw5Tl-g&s')] w-[15rem] h-[10rem] block justify-center rounded-[5px] box-border border-[0.5px] border-black shadow-lg">
-                    <div class="bg-gray-50 w-[40rem] h-[10rem] block justify-center rounded-[5px] box-border border-[0.5px] border-black shadow-lg">
-                        <h1 class="font-bold">p</h1>
-                        <p class="absolute ml-[0.7] font-medium">Text</p>
-                    </div>
-                    <button class="bg-zinc-400 text-white py-2 px-4 rounded-[5px] transition-transform transform hover:scale-105 hover:bg-zinc-900 focus:outline-none focus:ring-2 focus:ring-white">
-                        Edit 
-                        <i class='bx bx-edit-alt'></i>
-                      </button>
-                    </div>
-                
-                <div class="flex gap-[1.5rem] pt-[5rem] pb-[2rem] px-[10rem] bg-gray-200 w-screen">
-                        <div class="bg-slate-400 w-[15rem] h-[10rem] block justify-center rounded-[5px] box-border border-[0.5px] border-black shadow-lg"></div>
-                        <div class="bg-gray-50 w-[40rem] h-[10rem] block justify-center rounded-[5px] box-border border-[0.5px] border-black shadow-lg">
-                            <h1 class="font-bold">p</h1>
-                            <p class="absolute ml-[0.7] font-medium">Text</p>
+    <img class=" bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShPq0XGv-4wIxCVB_qHi42ZqrmFr-Aw5Tl-g&s')] w-[15rem] h-[10rem] block justify-center rounded-[5px]  shadow-lg">
+    <div class="bg-gray-50 w-[40rem] h-[10rem] block justify-center rounded-[5px] shadow-lg">
+        <h1 class="font-bold">p</h1>
+        <p class="absolute ml-[0.7] font-medium mx-5">Text</p>
+    </div>
+    <button class="bg-zinc-400 text-white py-2 px-4 rounded-[5px] transition-transform transform hover:scale-105 hover:bg-zinc-900 focus:outline-none focus:ring-2 focus:ring-white">
+        Edit 
+        <i class='bx bx-edit-alt'></i>
+      </button>
+    </div>
 
-                        </div>
-                        <button class="bg-zinc-400 text-white py-2 px-4 rounded-[5px] transition-transform transform hover:scale-105 hover:bg-zinc-900 focus:outline-none focus:ring-2 focus:ring-white">
-                            Edit
-                            <i class='bx bx-edit-alt'></i>
-                          </button>
-                        </div>
-                    
-                <div class="flex gap-[1.5rem] pt-[5rem] pb-[2rem] px-[10rem] bg-gray-200 w-screen">
-                        <div class="bg-slate-400 w-[15rem] h-[10rem] block justify-center rounded-[5px] box-border border-[0.5px] border-black shadow-lg"></div>
-                        <div class="bg-gray-50 w-[40rem] h-[10rem] block justify-center rounded-[5px] box-border border-[0.5px] border-black shadow-lg">
-                            <h1 class="font-bold">p</h1>
-                            <p class="absolute ml-[0.7] font-medium">Text</p>
 
-                        </div>
-                        <button class="bg-zinc-400 text-white py-2 px-4 rounded-[5px] transition-transform transform hover:scale-105 hover:bg-zinc-900 focus:outline-none focus:ring-2 focus:ring-white">
-                            Edit
-                            <i class='bx bx-edit-alt'></i>
-                          </button>
-                        </div>
-                <div class="flex gap-[1.5rem] pt-[5rem] pb-[2rem] px-[10rem] bg-gray-200 w-screen">
-                        <div class="bg-slate-400 w-[15rem] h-[10rem] block justify-center rounded-[5px] box-border border-[0.5px] border-black shadow-lg"></div>
-                        <div class="bg-gray-50 w-[40rem] h-[10rem] block justify-center rounded-[5px] box-border border-[0.5px] border-black shadow-lg">
-                            <h1 class="font-bold">p</h1>
-                            <p class="absolute ml-[0.7] font-medium">Text</p>
-
-                        </div>
-                        <button class="bg-zinc-400 text-white py-2 px-4 rounded-[5px] transition-transform transform hover:scale-105 hover:bg-zinc-900 focus:outline-none focus:ring-2 focus:ring-white">
-                            Edit
-                            <i class='bx bx-edit-alt'></i>
-                          </button>
-                        </div>
-                <div class="flex gap-[1.5rem] pt-[5rem] pb-[2rem] px-[10rem] bg-gray-200 w-screen">
-                        <div class="bg-slate-400 w-[15rem] h-[10rem] block justify-center rounded-[5px] box-border border-[0.5px] border-black shadow-lg"></div>
-                        <div class="bg-gray-50 w-[40rem] h-[10rem] block justify-center rounded-[5px] box-border border-[0.5px] border-black shadow-lg">
-                            <h1 class="font-bold">p</h1>
-                            <p class="absolute ml-[0.7] font-medium">Text</p>
-
-                        </div>
-                        <button class="bg-zinc-400 text-white py-2 px-4 rounded-[5px] transition-transform transform hover:scale-105 hover:bg-zinc-900 focus:outline-none focus:ring-2 focus:ring-white">
-                            Edit
-                            <i class='bx bx-edit-alt'></i>
-                          </button>
-                        </div>
-                <div class="flex gap-[1.5rem] pt-[5rem] pb-[2rem] px-[10rem] bg-gray-200 w-screen">
-                        <div class="bg-slate-400 w-[15rem] h-[10rem] block justify-center rounded-[5px] box-border border-[0.5px] border-black shadow-lg"></div>
-                        <div class="bg-gray-50 w-[40rem] h-[10rem] block justify-center rounded-[5px] box-border border-[0.5px] border-black shadow-lg">
-                            <h1 class="font-bold">p</h1>
-                            <p class="absolute ml-[0.7] font-medium">Text</p>
-
-                        </div>
-                        <button class="bg-zinc-400 text-white py-2 px-4 rounded-[5px] transition-transform transform hover:scale-105 hover:bg-zinc-900 focus:outline-none focus:ring-2 focus:ring-white">
-                            Edit
-                            <i class='bx bx-edit-alt'></i>
-                          </button>
-                        </div>
-                <div class="flex gap-[1.5rem] pt-[5rem] pb-[2rem] px-[10rem] bg-gray-200 w-screen">
-                        <div class="bg-slate-400 w-[15rem] h-[10rem] block justify-center rounded-[5px] box-border border-[0.5px] border-black shadow-lg"></div>
-                        <div class="bg-gray-50 w-[40rem] h-[10rem] block justify-center rounded-[5px] box-border border-[0.5px] border-black shadow-lg">
-                            <h1 class="font-bold">p</h1>
-                            <p class="absolute ml-[0.7] font-medium">Text</p>
-
-                        </div>
-                        <button class="bg-zinc-400 text-white py-2 px-4 rounded-[5px] transition-transform transform hover:scale-105 hover:bg-zinc-900 focus:outline-none focus:ring-2 focus:ring-white">
-                            Edit
-                            <i class='bx bx-edit-alt'></i>
-                          </button>
-                        </div>
-                        
+  
+  
+  
     </section>
 @endsection
