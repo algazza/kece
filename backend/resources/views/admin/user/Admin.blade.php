b2e62f163e4c84ce5543675528fdad9fd15fa991
howienathan <jonathanaugta3@gmail.com>
2024-08-19 08:13:50 +0700

    19/08/2024 08.13 nat

--- backend/resources/views/admin/user/Admin.blade.php
+++ backend/resources/views/admin/user/Admin.blade.php
@@ -3,7 +3,7 @@
 <section class="box-border p-0 m-0 bg-gray-200 text-center justify-center items-center h-screen font-poppins ">
    
     <section class="pt-[5rem] px-[5rem]">
-    <div class="bg-white my-20 mx-8 p-4 shadow border rounded w-full overflow-x-auto"> 
+    <div class="bg-white my-20 mx-8 p-4 shadow border rounded-5px w-full overflow-x-auto"> 
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTugiIkfShiBZQjxJ7sgDqrECI9ed3CQbdj0A&s" class="mx-auto text-center  rounded-full h-32 w-32 mb-4 object-cover" alt="profile-img" title="profile-img"/>
         <h1 class="text-center text-gray-900 text-2xl">Wahitukam</h1>
         <div>
