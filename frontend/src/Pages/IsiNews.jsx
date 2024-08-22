87d41d3aa6205ce50b867cc7a0e6170cbc0063b2
algazza <fadhilaswadira72564@gmail.com>
2024-08-22 10:31:11 +0700

    vranatutut

--- frontend/src/Pages/IsiNews.jsx
+++ frontend/src/Pages/IsiNews.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
-import NewsTemplate from "../Components/NewsTemplate";
 import styles from "../data/style";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
