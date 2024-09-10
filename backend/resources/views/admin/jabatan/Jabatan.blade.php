6dc0fc183663e6ca4c6db9230630a1b0112a5971
howienathan <jonathanaugta3@gmail.com>
2024-09-10 08:36:25 +0700

    jabatan

--- backend/resources/views/admin/jabatan/Jabatan.blade.php
+++ backend/resources/views/admin/jabatan/Jabatan.blade.php
@@ -61,8 +61,13 @@
                     </p>
                 </div>
             </a>
+<<<<<<< HEAD
          @endforeach
     </section>
+=======
+            @endforeach 
+        </section>
+>>>>>>> c7328798 (jabatan)
     <a href="{{ route('manajemen.add') }}" class="flex justify-end fixed bottom-10 right-10">
         <div class="flex items-center justify-center w-12 h-12 cursor-pointer hover:bg-slate-200 hover:scale-105 duration-300 border-2 border-black text-black rounded-full shadow-md">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
