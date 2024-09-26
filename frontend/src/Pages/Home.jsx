2fa188570f6f8ee865ecb4eb0f6619591256695c
algazza <fadhilaswadira72564@gmail.com>
2024-09-26 09:20:51 +0700

    meeting

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -32,6 +32,7 @@ import SimulasiCalcDeposito from "../Components/Simulasi/SimulasiCalcDeposito";
 import SimulasiCalcTabungan from "../Components/Simulasi/SimulasiCalcTabungan";
 import RateBunga from "../Components/Modal/RateBunga";
 import { localhostLink } from "../helper/localhost";
+import YoutubePopup from "../Components/Modal/YoutubePopup";
 
 const menuHome = [
   {
@@ -323,35 +324,7 @@ const Home = () => {
         </div>
 
         {openModal && (
-          <div className="w-screen h-dvh top-0 left-0 bottom-0 right-0 fixed z-40">
-            <div
-              className="w-screen h-dvh top-0 left-0 bottom-0 right-0 fixed bg-gray-900/50 backdrop-blur-sm"
-              onClick={handleModal}
-            >
-              <div
-                className="absolute left-1/2 top-[40%] -translate-x-1/2 -translate-y-1/4 bg-primary p-12"
-                onClick={(e) => e.stopPropagation()}
-              >
-                <div
-                  className="absolute right-2 top-2 cursor-pointer"
-                  onClick={handleModal}
-                >
-                  <CloseIcon />
-                </div>
-                <iframe
-                  width="560"
-                  height="315"
-                  src="https://www.youtube.com/embed/QfhNO1_GSEM?si=XYAIDIf6xs0zuBg6"
-                  title="YouTube video player"
-                  frameborder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                  referrerpolicy="strict-origin-when-cross-origin"
-                  allowfullscreen
-                  className="w-[18rem] h-[11rem] sm:w-[40rem] sm:h-[22rem]"
-                ></iframe>
-              </div>
-            </div>
-          </div>
+          <YoutubePopup handleModal={handleModal}/>
         )}
       </section>
 
