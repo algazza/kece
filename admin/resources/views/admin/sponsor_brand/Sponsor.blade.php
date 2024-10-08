c9aab5d9458d2daa0cbb0de4b9f7d691659c5464
howienathan <jonathanaugta3@gmail.com>
2024-10-08 14:52:37 +0700

    brand sponsor

--- admin/resources/views/admin/sponsor_brand/Sponsor.blade.php
+++ admin/resources/views/admin/sponsor_brand/Sponsor.blade.php
@@ -5,83 +5,16 @@
     <section id="testimonials" aria-label="What our customers are saying" class="md:mr-[5rem] sm2:mr-[3rem] xss:mr-[1rem] md:ml-[8rem] sm2:ml-[4rem] xss:ml-[3rem] pt-[5rem]">
         <div class=" mx-auto max-w-full px-5 sm:px-6 lg:px-6">
             <div class="bg-white py-4 max-w-full rounded-xl shadow-sm">
-                <h1 class="text-center text-xl font-semibold">Daftar Permintaan brand</h1>
-            </div>
-            <div class="block sss:hidden">
-                <div class=" grid grid-cols-1 lg:grid-cols-4 gap-2 mx-auto my-8 md:text-center">
-                    <a href="{{ route('sponsor.index') }}"
-                    class="w-full justify-center font-medium rounded-md px-5 py-2 border bg-white text-gray-800 border-gray-200 cursor-pointer hover:bg-gray-900 hover:scale-105 hover:text-white duration-300">
-                    All
-                </a>
-                <a href="{{ route('sponsor.index', ['jenis_sponsor' => 'MMT']) }}"
-                    class="w-full  justify-center font-medium rounded-md px-5 py-2 border bg-white text-gray-800 border-gray-200 cursor-pointer hover:bg-gray-900 hover:scale-105 hover:text-white duration-300">
-                    MMT
-                </a>
-
-                <a href="{{ route('sponsor.index', ['jenis_sponsor' => 'Tempat Tissue / Gelas']) }}"
-                    class="w-full justify-center font-medium rounded-md px-5 py-2 border bg-white text-gray-800 border-gray-200 cursor-pointer hover:bg-gray-900 hover:scale-105 hover:text-white duration-300">
-                    Tempat Tissue / Gelas
-                </a>
-
-                <a href="{{ route('sponsor.index', ['jenis_sponsor' => 'Nomor Meja']) }}"
-                    class="w-full justify-center font-medium rounded-md px-5 py-2 border bg-white text-gray-800 border-gray-200 cursor-pointer hover:bg-gray-900 hover:scale-105 hover:text-white duration-300">
-                    Nomor Meja
-                </a>
+                <h1 class="text-center text-xl font-semibold">Daftar Permintaan Sponsor</h1>
             </div>
         </div>
-    </div>
-    <ul role="list"
-    class="mx-auto mt-8 grid max-w-2xl xss:ml-[1rem] xs:ml-[1rem] ml-[5rem]  grid-cols-1 gap-6 sm:gap-8 lg:mt-20 lg:max-w-none lg:grid-cols-3">
-    @foreach ($sponsor as $item)
-
-    <div class="relative mx-auto max-w-md rounded-lg bg-gradient-to-tr from-white to-white p-0.5 shadow-lg">
-        <div class="bg-white p-7 rounded-md">
-            <div class="flex">
-                <div class="font-display text-base text-slate-900 font-normal">{{ $item->bidang_usaha }}</div>
-                <div class="px-2 border-r-2 border-gray-200"></div>
-                <div class="font-display font-semibold text-base text-slate-900 px-2">{{ $item->jenis_sponsor }}</div>
-            </div>
-            <div class="py-2">
-                <div class="flex">
-                    <div>
-                        <p class="text-lg tracking-tight text-slate-900 py-[6px]">Name</p>
-                        <p class="text-lg tracking-tight text-slate-900 py-[6px]">Nik</p>
-                        <p class="text-lg tracking-tight text-slate-900 py-[6px]">Email</p>
-                        <p class="text-lg tracking-tight text-slate-900 py-[6px]">Alamat</p>
-                        <p class="text-lg tracking-tight text-slate-900 py-[6px]">No Hp</p>
-                        <p class="text-lg tracking-tight text-slate-900 py-[6px]">Usaha</p>
-                        <p class="text-lg tracking-tight text-slate-900 py-[6px]">Notes</p>
-                        <p class="text-lg tracking-tight text-slate-900 py-[6px]">Proposal :</p>
-                    </div>
-                    <div>
-                        <p class="text-lg tracking-tight text-slate-900 py-[6px] px-3"> : {{ $item->nama }}</p>
-                        <p class="text-lg tracking-tight text-slate-900 py-[6px] px-3"> : {{ $item->nik }}</p>
-                        <p class="text-lg tracking-tight text-slate-900 py-[6px] px-3"> : {{ $item->email }}</p>
-                        <p class="text-lg tracking-tight text-slate-900 py-[6px] px-3"> : {{ $item->alamat }}</p>
-                        <p class="text-lg tracking-tight text-slate-900 py-[6px] px-3"> : {{ $item->no_handphone }}</p>
-                        <p class="text-lg tracking-tight text-slate-900 py-[5px] px-3"> : {{ $item->nama_usaha }}</p>
-                        <div class="flex flex-col">
-                            <p class="text-lg tracking-tight text-slate-900 py-[6px] px-3"> : {{ $item->catatan }}</p>
-                            <p class="text-lg tracking-tight text-slate-900 py-[6px] px-3">  <a href="{{ asset('image/public/sponsor/' . $item->pdf) }}" download class="flex items-center px-3 py-1 bg-white text-black hover:text-white rounded-lg shadow-lg hover:bg-red-600 transition">
-                                <i class='bx bxs-file-pdf'></i>
-                                <span>Download PDF</span>
-                            </a></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-              </div>
-              @endforeach
-            </ul>
-            ini jadi brand
-            <div class="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 md2:grid-cols-2 sm:grid-cols-1 gap-1">
-                <div class="bg-white rounded-3xl mx-4 md:mx-auto max-w-screen-md border-2 border-gray-400 mt-16">
-                    <div class="px-8 py-6 md:p-10">
-                        <div class="flex">
-                        <div class="font-display text-base text-slate-900 font-normal">Bidang Usaha</div>
+        <div class="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 md2:grid-cols-2 sm:grid-cols-1 gap-1">
+            <div class="bg-white rounded-3xl mx-4 md:mx-auto max-w-screen-md border-2 border-gray-400 mt-16">
+                <div class="px-8 py-6 md:p-10">
+                    <div class="flex">
+                        <div class="font-display text-base text-slate-900 font-normal">Dari <b>jam</b></div>
                         <div class="px-2 border-r-2 border-gray-200"></div>
-                        <div class="font-display font-semibold text-base text-slate-900 px-2">Jenis sponsor</div>
+                        <div class="font-display text-base text-slate-900 px-2">Hingga <b>jam</b></div>
                       </div>
                     <h2 class="text-3xl md:text-4xl font-medium leading-tight text-gray-800 mb-6">El Rakai Omar Wahid</h2>
                     <p class="text-base md:text-lg font-normal leading-normal text-gray-800 mb-6">Elrakkaiomarhawid@gmail.com</p>
@@ -96,9 +29,9 @@
             <div class="bg-white rounded-3xl mx-4 md:mx-auto max-w-screen-md border-2 border-gray-400 mt-16">
                 <div class="px-8 py-6 md:p-10">
                     <div class="flex">
-                        <div class="font-display text-base text-slate-900 font-normal">Bidang Usaha</div>
+                        <div class="font-display text-base text-slate-900 font-normal">Dari <b>jam</b></div>
                         <div class="px-2 border-r-2 border-gray-200"></div>
-                        <div class="font-display font-semibold text-base text-slate-900 px-2">Jenis sponsor</div>
+                        <div class="font-display text-base text-slate-900 px-2">Hingga <b>jam</b></div>
                       </div>
                     <h2 class="text-3xl md:text-4xl font-medium leading-tight text-gray-800 mb-6">El Rakai Omar Wahid</h2>
                     <p class="text-base md:text-lg font-normal leading-normal text-gray-800 mb-6">Elrakkaiomarhawid@gmail.com</p>
@@ -113,9 +46,9 @@
             <div class="bg-white rounded-3xl mx-4 md:mx-auto max-w-screen-md border-2 border-gray-400 mt-16">
                 <div class="px-8 py-6 md:p-10">
                     <div class="flex">
-                        <div class="font-display text-base text-slate-900 font-normal">Bidang Usaha</div>
+                        <div class="font-display text-base text-slate-900 font-normal">Dari <b>jam</b></div>
                         <div class="px-2 border-r-2 border-gray-200"></div>
-                        <div class="font-display font-semibold text-base text-slate-900 px-2">Jenis sponsor</div>
+                        <div class="font-display text-base text-slate-900 px-2">Hingga <b>jam</b></div>
                       </div>
                     <h2 class="text-3xl md:text-4xl font-medium leading-tight text-gray-800 mb-6">El Rakai Omar Wahid</h2>
                     <p class="text-base md:text-lg font-normal leading-normal text-gray-800 mb-6">Elrakkaiomarhawid@gmail.com</p>
--- admin/resources/views/admin/sponsor_brand/SponsorData.blade.php
+++ admin/resources/views/admin/sponsor_brand/SponsorData.blade.php
@@ -2,13 +2,10 @@
 @section('content')
 <section class="box-border p-0 m-0 bg-gray-200 text-center justify-center items-center h-screen font-poppins overflow-x-hidden">
     <section class="ml-[5%] rounded-[5px] py-[3rem] text-left justify-center flex flex-col">
-
-
         <div class="mx-14 mt-10 border-2 bg-white border-gray-300 rounded-lg">
             <div class=" text-center text-1xl text-gray-400 font-bold mt-[2rem]"></div>
             <div class="text-center text-3xl font-bold"></div>
             <div class="p-8">
-      ini di ganti jadi brand data
     <div class="grid grid-cols-1 sm:flex gap-4">
       <div class="w-1/2">
         <label for="no_handphone" class="block text-gray-700 font-semibold mb-1">Nama</label>
@@ -25,27 +22,24 @@
         <input type="text" id="no_handphone" readonly name="no_handphone" class="block w-full rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm" />
       </div>
       <div class="w-1/2">
-        <label for="no_handphone" class="block text-gray-700 font-semibold mb-1">Nama Acara</label>
+        <label for="no_handphone" class="block text-gray-700 font-semibold mb-1">Nama Usaha</label>
         <input type="text" id="no_handphone" readonly name="no_handphone" class="block w-full rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm" />
       </div>
     </div>
     <div class="my-2 grid grid-cols-1 sm:flex gap-4">
       <div class="w-1/2">
-        <label for="no_handphone" class="block text-gray-700 font-semibold mb-1">Tanggal Awal Acara</label>
-        <input type="date" id="no_handphone" readonly name="no_handphone" class="block w-full rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm" />
+        <label for="no_handphone" class="block text-gray-700 font-semibold mb-1">Nik</label>
+        <input type="text" id="no_handphone" readonly name="no_handphone" class="block w-full rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm" />
       </div>
       <div class="w-1/2">
-        <label for="no_handphone" class="block text-gray-700 font-semibold mb-1">Tanggal Akhir Acara</label>
-        <input type="date" id="no_handphone" readonly name="no_handphone" class="block w-full rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm" />
+        <label for="no_handphone" class="block text-gray-700 font-semibold mb-1">Pekerjaan</label>
+        <input type="text" id="no_handphone" readonly name="no_handphone" class="block w-full rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm" />
       </div>
     </div>
     <div class="my-4 grid grid-cols-1 sm:flex gap-4">
       <div class="w-full">
-        <label for="" class="block text-gray-700 font-semibold mb-1">Lokasi</label>
-        <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3959.726844710513!2d110.46356827475826!3d-7.041353492960724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e708d4a93de1f07%3A0xc0c45f754cb4c6!2sBank%20Bpr%20Arto%20Moro!5e0!3m2!1sid!2sid!4v1727941706882!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
-        width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" class="rounded-lg w-full">
-    </iframe>
+        <label for="" class="block text-gray-700 font-semibold mb-1">Alamat</label>
+        <textarea name="textarea" id="text" class=" w-full resize-none rounded-md border border-slate-300 p-5 text-gray-500" id="readonlyinput" readonly></textarea>
       </div>
     </div>
 
