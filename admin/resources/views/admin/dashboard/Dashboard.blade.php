1ad5d515a23d0f3b9431a083327eb6e7cbcb7537
howienathan <jonathanaugta3@gmail.com>
2024-10-11 13:05:32 +0700

    last

--- admin/resources/views/admin/dashboard/Dashboard.blade.php
+++ admin/resources/views/admin/dashboard/Dashboard.blade.php
@@ -132,6 +132,20 @@
                     </a>
                 @endif
                 @if (Auth::user()->role == 'promosi' || Auth::user()->role == 'admin')
+                    <a href="{{ route('armor.index') }}">
+                        <div class="w-[23rem] sm:w-[35rem] h-[4.4rem] bg-gray-50 rounded-[5px] my-[0.6rem] box-border border-[0.5px] border-black shadow-md cursor-pointer hover:bg-gray-200 hover:scale-95 duration-300 sm:flex-row lg:flex-wrap lg:justify-between">
+                            <div class="flex">
+                                <div class="text-[3.5rem] mt-[-0.1rem] mx-[1.5rem]">
+                                    <i class='text-black bx bxs-home text-5xl' ></i>
+                                </div>
+                                <div class="block mt-[0.8rem]">
+                                    <h3 class="text-[1.1rem] font-bold pt-2">Armor Property</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                @endif
+                @if (Auth::user()->role == 'promosi' || Auth::user()->role == 'admin')
                     <a href="{{ route('promo.index') }}">
                         <div class="w-[23rem] sm:w-[35rem] h-[4.4rem] bg-gray-50 rounded-[5px] my-[0.6rem] box-border border-[0.5px] border-black shadow-md cursor-pointer hover:bg-gray-200 hover:scale-95 duration-300 sm:flex-row lg:flex-wrap lg:justify-between">
                             <div class="flex">
